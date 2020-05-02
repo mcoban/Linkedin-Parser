f7f22b6ca6dd9712c9d2bbcb801bd0217141860f
Müslüm Çoban <mcoban86@gmail.com>
2020-05-02 11:46:50 +0300

    header is in progress

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
+  },
+  created () {
+    console.log('app is loaded')
+  }
+}
+</script>
+
 <style lang="scss">
+html, body {
+  padding: 0;
+  margin: 0;
+  min-height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #e8e8e8;
+  min-height: 100vh;
 }
+.container {
+  margin: auto;
+  width: 100%;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  @media (min-width: 1200px) {
+    max-width: 1000px;
   }
+}
+a {
+  text-decoration: none;
 }
 </style>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="header">
-    this is header
+    <div class="container">
+      <div class="flex">
+        <a class="header__brand" href="#"><img src="../assets/header-logo.png" height="34" alt="Logo"></a>
+        <input type="text" class="header__search" placeholder="Search a brand, company or persona">
+        <nav></nav>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  padding: .6em 0;
+  background: #283e4a;
+  a {
+    color: #fff;
+  }
+  .header__brand {
+    img {
+      float: left;
+    }
+  }
+  .header__search {
+    height: 34px;
+    border-radius: 2px;
+    border: none;
+    padding: 0 .7rem;
+    background: #e1e9ee;
+    color: rgba(40,62,74,.75);
+    outline: none;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    min-width: 320px;
+    font-size: .9rem;
+  }
+}
+
+.flex {
+  display: flex;
+
+  nav {
+    margin-left: auto;
+  }
+}
+</style>
